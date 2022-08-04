c94ba7a82d9d8ef1cb4e429b9fd4c5e75a91c1b5
Joanne <linjoanne823@gmail.com>
2022-08-03 20:43:59 -0700

    CreatedAPI to GET favourite recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -45,4 +45,37 @@ router.get("/:recipeId", (req, res) => {
     .catch(() => res.status(500).send("error"));
 });
 
+const favouriteRecipes = [
+  {
+    title: "Berry Banana Breakfast Smoothie",
+    image: "https://spoonacular.com/recipeImages/715497-312x231.jpg",
+    readyInMinutes: "5",
+    servings: "1",
+    ingredients: "many ingredients",
+    instructions: "many instructions",
+  },
+  {
+    title: "Slow Cooker Red Beans and Rice",
+    image: "https://spoonacular.com/recipeImages/715493-556x370.jpg",
+    readyInMinutes: "20",
+    servings: "2",
+    ingredients: "many ingredients",
+    instructions: "many instructions",
+  },
+];
+
+router.get("/favourites/recipes", (req, res) => {
+  const favouriteRecipe = favouriteRecipes.map((element) => {
+    return {
+      title: element.title,
+      image: element.image,
+      readyInMinutes: element.readyInMinutes,
+      servings: element.servings,
+      ingredients: element.ingredients,
+      instructions: element.instructions,
+    };
+  });
+  res.json(favouriteRecipe);
+});
+
 module.exports = router;
